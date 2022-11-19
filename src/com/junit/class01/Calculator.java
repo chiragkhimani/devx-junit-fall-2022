fe5feba3ba7563b09bb4623639bfe470a46db53f
chiragkhimani <khimanichirag@gmail.com>
2022-11-19 08:37:02 +0530

    initial commit

---
+++ src/com/junit/class01/Calculator.java
@@ -0,0 +1,24 @@
+package com.junit.class01;
+
+public class Calculator {
+
+    public int sum(int a, int b) {
+        return a + b;
+    }
+
+    public int sub(int a, int b) {
+        return a - b;
+    }
+
+    public int mul(int a, int b) {
+        return a * b;
+    }
+
+    public int div(int a, int b) {
+        return a / b;
+    }
+
+    public int modulo(int a, int b) {
+        return a % b;
+    }
+}
