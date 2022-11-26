6e54f5a74a90c93bececc1ff9c65b304fe46791d
chiragkhimani <khimanichirag@gmail.com>
2022-11-26 08:00:11 +0530

    Beofre After

---
+++ src/com/junit/class01/JunitBeforeAndAfter.java
@@ -0,0 +1,34 @@
+package com.junit.class01;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class JunitBeforeAndAfter {
+
+        @Test
+        public void verifyUserCanLoginIntoTheApp(){
+            System.out.println("============ Test 1=====================");
+        }
+
+        @Test
+        public void verifyUserCannotLoginWithInvalidCred(){
+            System.out.println("============ Test 2=====================");
+        }
+
+        @Test
+        public void verifyUserCanLogout(){
+            System.out.println("============ Test 3=====================");
+
+        }
+
+        @Before
+        public void setUp(){
+            System.out.println("============ Before =============");
+        }
+
+        @After
+        public void cleanUp(){
+            System.out.println("============ After =============");
+        }
+}
